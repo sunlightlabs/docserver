02603d58203f60fe5b2cb977c323567b069b1c07
Joshua Ruihley <ruihley@gmail.com>
2009-08-12 21:58:37 +0000

    added congress_utils

---
+++ congress_utils.py
@@ -0,0 +1,15 @@
+import re
+
+# return list of bill numbers extracted from string
+def extract_legislation(haystack):
+    haystack = haystack.upper()
+    p = re.compile('S\.\s?CON\.\s?RES\.\s?\d{1,5}|H\.\s?CON\s?RES\.\s?\d{1,5}|S\.\s?J\.\s?RES\.\s\d{1,5}|H\.\s?J\.\s?RES\.\s\d{1,5}|S\.\s?RES\.\s?\d{1,5}|H\.\s?RES\.\s?\d{1,5}|H\.\s?\R\.\s?\d{1,5}|S\.\s?\d{1,5}')
+    needle_list = p.findall(haystack)
+    return needle_list
+    
+#returns number of Congress in session for provided year
+def congress_from_year(year):
+    if year < 1789:
+        return None
+    else:
+        return int((year-1789)/2) + 1
