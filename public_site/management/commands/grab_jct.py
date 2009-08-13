e5322e79c9fa8179e6368316b28f2ceaa7073abb
Joshua Ruihley <ruihley@gmail.com>
2009-08-13 03:27:37 +0000

    cleaned jct scraper

--- public_site/management/commands/grab_jct.py
+++ public_site/management/commands/grab_jct.py
@@ -1,36 +1,19 @@
-from django.core.management.base import NoArgsCommand
-import datetime, time
-import os
-import urllib2
 from BeautifulSoup import BeautifulSoup, SoupStrainer
+from congress_utils import *
 from docserver.public_site.models import Document, DocumentLegislation
-import re
+from django.core.management.base import NoArgsCommand
+from scrape_utils import *
+import datetime, time
+import urllib2
 
 class Command(NoArgsCommand):
     
     def handle_noargs(self, **options):
-        
-        # return list of bill numbers extracted from string
-        def extract_legislation(haystack):
-            if haystack:
-                haystack = haystack.upper()
-                p = re.compile('S\.\s?CON\.\s?RES\.\s?\d{1,5}|H\.\s?CON\s?RES\.\s?\d{1,5}|S\.\s?J\.\s?RES\.\s\d{1,5}|H\.\s?J\.\s?RES\.\s\d{1,5}|S\.\s?RES\.\s?\d{1,5}|H\.\s?RES\.\s?\d{1,5}|H\.\s?\R\.\s?\d{1,5}|S\.\s?\d{1,5}')
-                needle_list = p.findall(haystack)
-                return needle_list
-            else:
-                return []
-            
-        #returns number of Congress in session for provided year
-        def congress_from_year(year):
-            if year < 1789:
-                return None
-            else:
-                return int((year-1789)/2) + 1
-        
-        add_date = datetime.datetime.now()
         doc_type = "JCT"
+        file_type = "pdf"
         base_url = "http://jct.gov"
         page = urllib2.urlopen("http://www.jct.gov/publications.html?func=select&id=17")
+        add_date = datetime.datetime.now()
         
         soup = BeautifulSoup(page)
         doc_list = soup.findAll(attrs={"class":"jct_fileblock"})
@@ -41,40 +24,27 @@ class Command(NoArgsCommand):
             title = doc_soup.find('title')
             gov_id = title.string
             description = doc_soup.find('meta', attrs={'name':'description'})['content']
-            print description
             remository = doc_soup.findAll('div', attrs={'id':'remositoryfileinfo'})
             for info in remository:
                 original_url = "%s%s" % (base_url, info.findAll('a')[0]['href'])
-
                 date_str = info.findAll('dt')[1].nextSibling.nextSibling.contents[0].strip()
-                print date_str
                 try:
                     release_date = time.strftime('%Y-%m-%d', time.strptime(date_str, '%B %d, %Y'))
                 except:
                     release_date = None
-                print release_date
                 year = time.strptime(date_str, '%B %d, %Y')[0]
                 congress = congress_from_year(year)
                 title = description
                 bill_list = extract_legislation(title)
                 description = ""
                 local_file = ""
-                print title
   
                 matches = Document.objects.filter(doc_type=doc_type, gov_id=gov_id, release_date=release_date)
                 if len(matches) > 0:
                     pass
                 else:
                     if gov_id != None:
-                        try:
-                            file_name = "/var/www/data/docserver/jct/%s.pdf" % gov_id
-                            remote_file = urllib2.urlopen(original_url)
-                            local_file = open(file_name, "w")
-                            local_file.write(remote_file.read())
-                            local_file.close
-                        except:
-                            print "nope"
-
+                        local_file = archive_file(original_url, gov_id, doc_type, file_type)
                         doc = Document(gov_id=gov_id, release_date=release_date, add_date=add_date, title=title, 
                             description=description, doc_type=doc_type, original_url=original_url, local_file=local_file)
                         doc.save()
