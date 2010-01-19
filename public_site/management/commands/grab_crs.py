de628a5119cfea73a622977276e3cb1c3a3fba76
jroo <ruihley@gmail.com>
2010-01-19 16:28:12 +0000

    updated open crs importer to accommodate new open crs api

--- public_site/management/commands/grab_crs.py
+++ public_site/management/commands/grab_crs.py
@@ -14,49 +14,34 @@ class Command(NoArgsCommand):
         doc_type = "CRS"
         file_type = "pdf"
         
-        for i in range(0,1):
-            print "PAGE %s" % i
-            url = "http://opencrs.com/recent/feed.xml?key=%s&page=%s" % (OPEN_CRS_KEY, i)
-            print url
+        for i in range(1,2):
+            #print "PAGE %s" % i
+            url = "http://opencrs.com/api/reports/list.xml?key=%s&page=%s" % (OPEN_CRS_KEY, i)
+            #print url
             page = urllib2.urlopen(url)
             soup = BeautifulStoneSoup(page)
-            for document in soup.documents.findAll('document'):
+            for document in soup.response.findAll('resource'):
                 db.reset_queries()
-                order_code = document['order_code']
-                release_date = document['release_date']
-                print order_code
+                order_code = document.find('ordercode').contents[0]
+                release_date = document.find('releasedate').contents[0]
+                original_url = document.find('download_url').contents[0]
                 gov_id = "CRS_%s_%s" % (order_code, release_date.replace('-', ''))
-                title = document.title.contents[0]
+                title = document.find('title').contents[0]
                 description = ' '
                 try:
-                    original_url = document.locations.findAll('location')[0]['url']
-                    bill_list = []
-                    for bill in document.legislation.findAll('bill'):
-                        bill_list.append({'bill_id':bill['id'], 'congress':bill['congress']})
                     matches = Document.objects.filter(doc_type=doc_type, gov_id=gov_id, release_date=release_date)
                     if not matches:
-                        print "new"
+                        #print "new"
                         if gov_id:
                             local_file = archive_file(original_url, gov_id, doc_type, file_type)
                             full_text = pdf_extract_text(local_file, original_url)
                             doc = Document(gov_id=gov_id, release_date=release_date, add_date=add_date, title=title, 
-                                description=description, doc_type=doc_type, original_url=original_url, 
+                                description=description, doc_type=doc_type, original_url=download_url, 
                                 local_file=local_file, full_text=full_text)
+                            print doc
                             doc.save()
-                            for bill in bill_list:
-                                bill_dupe = DocumentLegislation.objects.filter(congress=bill['congress']).filter(bill_num=bill['bill_id']).filter(document=doc)
-                                if not bill_dupe:
-                                    bill = DocumentLegislation(congress=bill['congress'], bill_num=bill['bill_id'], document=doc)
-                                    bill.save()
                     else:
-                        print "dupe"
-                        doc = matches[0]
-                        for bill in bill_list:
-                            bill_dupe = DocumentLegislation.objects.filter(congress=bill['congress']).filter(bill_num=bill['bill_id']).filter(document=doc)
-                            if not bill_dupe:
-                                print "add bill %s" % bill
-                                bill = DocumentLegislation(congress=bill['congress'], bill_num=bill['bill_id'], document=doc)
-                                bill.save()
+                        pass
                 except:
                     pass
                 print gov_id
