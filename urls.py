764cc7fd3fca9f8ae9a79a11533f0e699f1f679f
Joshua Ruihley <ruihley@gmail.com>
2010-06-11 22:26:15 +0000

    fixed urls conflict

--- urls.py
+++ urls.py
@@ -14,7 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/(.*)', admin.site.root),
-<<<<<<< HEAD:urls.py
     (r'^$', 'public_site.views.index'),
     (r'^iphone/$', 'public_site.views.iphone'),
     (r'^search/$', 'public_site.views.search'),
@@ -23,14 +22,4 @@ urlpatterns = patterns('',
     (r'^bill/(?P<congress>\d+)-(?P<bill_type>\w+)-(?P<bill_id>\d+)/list.(?P<format>\w+)$', 'public_site.views.bill'),
     (r'^(?P<doc_type>\w+)/$', 'public_site.views.typelist'),
     (r'^(?P<doc_type>\w+)/list.(?P<format>\w+)$', 'public_site.views.typelist'),
-=======
-    (r'^$', 'docserver.public_site.views.index'),
-    (r'^iphone/$', 'docserver.public_site.views.iphone'),
-    (r'^search/$', 'docserver.public_site.views.search'),
-    (r'^timeline/(?P<congress>\d+)-(?P<bill_type>\w+)-(?P<bill_id>\d+)/$', 'docserver.public_site.views.timeline'),
-    (r'^bill/(?P<congress>\d+)-(?P<bill_type>\w+)-(?P<bill_id>\d+)/$', 'docserver.public_site.views.bill'),
-    (r'^bill/(?P<congress>\d+)-(?P<bill_type>\w+)-(?P<bill_id>\d+)/list.(?P<format>\w+)$', 'docserver.public_site.views.bill'),
-    (r'^(?P<doc_type>\w+)/$', 'docserver.public_site.views.typelist'),
-    (r'^(?P<doc_type>\w+)/list.(?P<format>\w+)$', 'docserver.public_site.views.typelist'),
->>>>>>> 22ccb1950febf3a0f37005b53b1c7bcc5f5dee03:urls.py
 )
